ed72c58f0dc9c78ba3b60ec51374cc2f4911cc56
Mankirat Singh <msingh2_be22@thapar.edu>
2023-09-29 23:52:53 +0530

    DS/Ass3/Q3 corrected
    Signed-off-by: Mankirat Singh <msingh2_be22@thapar.edu>

--- DS/Assignment 2/Q3.cpp
+++ DS/Assignment 2/Q3.cpp
@@ -3,12 +3,28 @@ using namespace std;
 
 int main() {
     int arr[7]={1,2,3,5,6,7,8};
-    int sum=0;
-    for(int i=0;i<7;i++){
-        sum+=arr[i];
+
+    // using sum formula
+    // int sum=0;
+    // for(int i=0;i<7;i++){
+    //     sum+=arr[i];
+    // }
+    // int n=arr[6];
+    // int sum2=(n*(n+1))/2;
+    // cout<<"Missing Number: "<<sum2-sum;
+    // return 0;
+
+    // using binary search
+    int low=0,high=6,mid;
+    while(low<=high){
+        mid=(low+high)/2;
+        if(arr[mid]==mid+1){
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
     }
-    int n=arr[6];
-    int sum2=(n*(n+1))/2;
-    cout<<"Missing Number: "<<sum2-sum;
+
+    cout<<"Missing Number: "<<low+1;
     return 0;
 }
